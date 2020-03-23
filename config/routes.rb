3184c3b396cbbd1469df12b70c54659f66b0d7c0
Guilherme Barbosa <guilhermebarbosa@Guilhermes-MacBook-Pro.local>
2020-03-22 21:29:59 -0300

    first

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'landing/index'
+
+  root 'landing#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :messages, only: [:new, :create]
+
+end
